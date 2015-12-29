b26b8b56d9d7390b4d6bb494ad7f0cf9f8c18ec8
eaaguirregiraldo <eaaguirregiraldo@gmail.com>
2015-12-29 16:58:19 -0500

    implementa funcionalidad de autenticar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  
- resources :users, only: [:new, :create]
 
+  root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
+  resources :products
+  
+  
   #get '/products', to: 'products#index'
   #get '/products/new', to: 'products#new', as: 'new_product'
   #post '/products', to: 'products#create'
@@ -10,7 +17,7 @@ Rails.application.routes.draw do
   #patch '/products/:id', to: 'products#update'
   #delete '/products/:id', to: 'products#destroy' 
 
-  resources :products
+  
 
-  root 'products#index'
+  
 end
