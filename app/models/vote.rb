b7b19be627373d1cf1db8404bf3afe7be3993ddc
eaaguirregiraldo <eaaguirregiraldo@gmail.com>
2016-01-08 15:42:26 -0500

    Implementa la funcionalidad de votos

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,16 +1,15 @@
 # == Schema Information
 #
-# Table name: comments
+# Table name: votes
 #
 #  id         :integer          not null, primary key
-#  body       :text
 #  product_id :integer
 #  user_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
-class Comment < ActiveRecord::Base
+class Vote < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 end
